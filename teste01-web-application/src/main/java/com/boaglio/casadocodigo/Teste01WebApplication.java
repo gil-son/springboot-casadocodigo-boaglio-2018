298e9bed5f168ec06b6efd474da5f0e1a732aad6
gil-son <gilszon@gmail.com>
2021-04-01 10:51:24 -0300

    Web Application

---
+++ teste01-web-application/src/main/java/com/boaglio/casadocodigo/Teste01WebApplication.java
@@ -0,0 +1,13 @@
+package com.boaglio.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste01WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste01WebApplication.class, args);
+	}
+
+}
